79990cc128b77791f5685a33cfd814a0f651120b
Vincent ROBERT <vincent.robert@cdfnancy.fr>
2021-01-22 14:18:59 +0100

    Projet initial

---
+++ exemple1.cpp
@@ -0,0 +1,20 @@
+/***************************
+
+*****************************/
+#include <iostream>	// bibliothèque de gestion des E/S
+#include <conio.h>	// gestion de la console (ici _getch())
+
+
+using namespace std;
+
+/*****************************
+Fonction principale
+*****************************/
+int main()	// Fonction principale
+{
+
+
+
+	_getch();	// attente d'appui sur une touche
+	return 0;	// fin du programme. Le code 0 est envoyé
+}
